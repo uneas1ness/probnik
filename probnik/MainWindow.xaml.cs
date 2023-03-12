d461cbe2b5c7841585baaf8c61f3546a54e5e16c
Крот <@DESKTOP-8HJGMHB>
2023-03-12 18:15:18 +0300

    Добавьте файлы проекта.

---
+++ probnik/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace probnik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            if (sender == open_btn)
+            {
+                imgCocnet.Content = Resources["Image_Open"];
+            }
+            else
+            {
+                imgCocnet.Content = Resources["Image_Close"];
+            }
+        }
+    }
+}
